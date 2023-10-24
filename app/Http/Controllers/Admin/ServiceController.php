dbb4ed5597d916527c0801661a5b7f1fd1e3a6b1
HammamZarefa <52674073+HammamZarefa@users.noreply.github.com>
2023-10-24 12:25:26 +0300

    Update ServiceController.php

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -309,16 +309,19 @@ class ServiceController extends Controller
     }
 
     public function SaleOrRent($id){
-        $is_for_sale = Product::find($id)->is_for_sale;
-        if($is_for_sale){
-            Product::find($id)->update(['status'=>'sale']);
-            $this->insertInInvoices(Product::find($id));
-            $this->send_event_notification( User::find(Product::find($id)->user->id),'', ' تم تغيير حالة منتجك الى بيع ' , 'Your product status has been changed to Sold' );
+        $product = Product::find($id);
+        if($product->user)
+        {
+        if($product->is_for_sale){
+            $product->update(['status'=>'sale']);
+            $this->insertInInvoices($product);
+            $this->send_event_notification($product->user->id),'', ' تم تغيير حالة منتجك الى بيع ' , 'Your product status has been changed to Sold' );
         }
         else{
-            Product::find($id)->update(['status'=>'rent']);
-            $this->insertInInvoices(Product::find($id));
-            $this->send_event_notification( User::find(Product::find($id)->user->id) ,'', ' تم تغيير حالة منتجك الى بيع ' , 'Your product status has been changed to Sold' );
+            $product->update(['status'=>'rent']);
+            $this->insertInInvoices($product);
+            $this->send_event_notification( $product->user->id) ,'', ' تم تغيير حالة منتجك الى بيع ' , 'Your product status has been changed to Sold' );
+        }
         }
         $notify[] = ['success', 'Status updated!'];
         return back()->withNotify($notify);    }
